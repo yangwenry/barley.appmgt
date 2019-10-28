ae9f3d50f0c1e59aa2528780f53c9dd6cd1db3e6
yangwenry <yangwenry@gmail.com>
2019-10-28 09:29:01 +0900

    디폴트 롤 변경 (creator, publisher)
    prefix로 app- 추가

--- barley.appmgt.impl/src/main/java/barley/appmgt/impl/AppMConstants.java
+++ barley.appmgt.impl/src/main/java/barley/appmgt/impl/AppMConstants.java
@@ -660,8 +660,8 @@ public final class AppMConstants {
     public static final String NO_MATCHING_AUTH_SCHEME = "noMatchedAuthScheme";
 
     public static final String EVERYONE_ROLE = "internal/everyone";
-    public static final String CREATOR_ROLE = "internal/creator";
-    public static final String PUBLISHER_ROLE = "internal/publisher";
+    public static final String CREATOR_ROLE = "internal/app-creator";
+    public static final String PUBLISHER_ROLE = "internal/app-publisher";
     public static final String STORE_ADMIN_ROLE = "internal/store-admin";
     public static final String ANONYMOUS_ROLE = "system/wso2.anonymous.role";
 
